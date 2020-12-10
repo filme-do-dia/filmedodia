b04fe953710f6a0b67762c93aa24077b76d8e837
Viclss <vtr.08@hotmail.com>
2020-12-10 18:38:08 -0300

    Pequenas alteraçoes e teste confirm de adult

--- scripts/variaveis.js
+++ scripts/variaveis.js
@@ -3,4 +3,6 @@ const baseUrl = 'https://api.themoviedb.org/3';
 const language = 'pt-BR'
 const page = Math.floor(Math.random() * 500) + 1;
 const position = Math.floor(Math.random() * 20);
-const url = `${baseUrl}/discover/movie?api_key=${api_key}&language=${language}&sort_by=popularity.desc&include_adult=true&include_video=false&page=${page}`;
+// const adult = confirm("Deseja Visualizar filmes 18 + (contem filmes adultos, terror e similares)");
+// const url = `${baseUrl}/discover/movie?api_key=${api_key}&language=${language}&sort_by=popularity.desc&include_adult=${adult}&include_video=false&page=${page}`;
+const url = `${baseUrl}/discover/movie?api_key=${api_key}&language=${language}&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`;
